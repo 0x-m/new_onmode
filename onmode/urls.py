d02fecbb63f3a89894fe50b74fa53fbe6474704e
0x-m <m.ghaedi.74@gmail.com>
2022-04-21 14:44:09 +0430

    adding contactus aboutus certs

--- onmode/urls.py
+++ onmode/urls.py
@@ -1,4 +1,5 @@
 
+from ssl import cert_time_to_seconds
 from django.contrib import admin
 from django.http import HttpRequest
 from django.shortcuts import render
@@ -13,17 +14,17 @@ def about_us(request: HttpRequest):
     return render(request, 'aboutus.html')
 
 def contact_us(request: HttpRequest):
-    return render(request, 'index.html')
+    return render(request, 'contactus.html')
 
-def rules(request: HttpRequest):
-    return render(request, 'index.html')
+def cert(request: HttpRequest):
+    return render(request, 'certs.html')
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/aboutus', about_us, name='aboutus' ),
-    path('/contactus', contact_us, name='contactus' ),
-    path('/rules', rules, name='rules' ),
+    path('aboutus/', about_us, name='aboutus' ),
+    path('contactus/', contact_us, name='contactus' ),
+    path('cert/', cert, name='cert' ),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users'), )
 ]
