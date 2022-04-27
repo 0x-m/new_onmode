45cda1b85ca7d986449fa339fc955e35c1845d0d
0x-m <m.ghaedi.74@gmail.com>
2022-04-27 20:56:03 +0430

    added: index blogpost sliderphoto photo deleted while deleting instance

--- index/models.py
+++ index/models.py
@@ -1,9 +1,10 @@
 from turtle import title
 from django.db import models
+from django.dispatch import receiver
 
 from catalogue.models import Collection, Shop
 from django_quill.fields import QuillField
-
+from django.db.models.signals import pre_delete
 
 
 class SliderPhoto(models.Model):
@@ -12,10 +13,16 @@ class SliderPhoto(models.Model):
     alt = models.CharField(max_length=255, blank=True)
     link = models.URLField(null=True, blank=True)
     precedence = models.PositiveIntegerField(default=0)
- 
+
+    @receiver(pre_delete)
+    def delet_photo(sender, instance, **kwargs):
+        if instance.photo:
+            instance.photo.delete()
+
 
 class About(models.Model):
     content = QuillField()
+
 
 class Law(models.Model):
     content = QuillField()
@@ -33,20 +40,20 @@ class SiteInfo(models.Model):
     jumbo_message = models.TextField(max_length=1000, blank=True)
     jumbo_button_text = models.CharField(max_length=50, blank=True)
     jumb_button_url = models.URLField(null=True, blank=True)
-    
+
     tel_line_1 = models.CharField(max_length=20, blank=True)
     tel_line_2 = models.CharField(max_length=20, blank=True)
     mobile_phone = models.CharField(max_length=20, blank=True)
     address = models.TextField(max_length=1000, blank=True)
     email = models.EmailField(blank=True, null=True)
-    
+
     instagram = models.URLField(blank=True, null=True)
     linkedin = models.URLField(blank=True, null=True)
     facebook = models.URLField(blank=True, null=True)
     telegram = models.URLField(blank=True, null=True)
     whatsapp = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
- 
+
 
 class BlogPost(models.Model):
     photo = models.ImageField(null=True)
@@ -54,8 +61,18 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=255, blank=True)
     url = models.URLField(blank=True, null=True)
 
+    @receiver(pre_delete)
+    def delet_photo(sender, instance, **kwargs):
+        if instance.photo:
+            instance.photo.delete()
+
+
 class ShopSet(models.Model):
     shops = models.ManyToManyField(to=Shop)
     title = models.CharField(max_length=255, blank=True)
     slogan = models.CharField(max_length=255, blank=True)
-    
+
+    @receiver(pre_delete)
+    def delet_photo(sender, instance, **kwargs):
+        if instance.photo:
+            instance.photo.delete()
