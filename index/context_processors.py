5ef105f3ff84a9c74791969c4b77673434ec7f46
0x-m <m.ghaedi.74@gmail.com>
2022-05-10 22:34:49 +0430

    bugfix: index.contextprocessor.siteinfo null constrain

--- index/context_processors.py
+++ index/context_processors.py
@@ -5,7 +5,7 @@ def info(request):
     siteinfo = SiteInfo.objects.first()
     return {
         'siteinfo': siteinfo,
-        'GOOGLE_TAG_ID': siteinfo.GOOGLE_TAG_ID,
+        'GOOGLE_TAG_ID': siteinfo.GOOGLE_TAG_ID if siteinfo else '',
         'certs' : Certificate.objects.filter(show_on_index=True).all(),
     }
 
