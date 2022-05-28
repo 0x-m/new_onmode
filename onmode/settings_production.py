1a8ff9f966c1168687895a1e16533f93915316b3
0x-m <m.ghaedi.74@gmail.com>
2022-05-28 20:42:02 +0430

    logger turned on

--- onmode/settings_production.py
+++ onmode/settings_production.py
@@ -83,6 +83,9 @@ DATABASES = {
         'NAME':  config('DB_NAME'),
         'USER': config('DB_USER_NAME'),
         'PASSWORD': config('DB_PASSWORD'),
+        'OPTIONS': {
+            'sql_mode': ''
+        }
 
     }
 }
@@ -207,42 +210,42 @@ IMPORT_EXPORT_USE_TRANSACTIONS = False
 
 
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {
-#             'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-#                        'pathname=%(pathname)s lineno=%(lineno)s ' +
-#                        'funcname=%(funcName)s %(message)s'),
-#             'datefmt': '%Y-%m-%d %H:%M:%S'
-#         },
-#         'simple': {
-#             'format': '%(levelname)s %(message)s'
-#         }
-#     },
-#     'handlers': {
-#         'null': {
-#             'level': 'DEBUG',
-#             'class': 'logging.NullHandler',
-#         },
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'verbose'
-#         },
-#         'logfile': {
-#             'class': 'logging.FileHandler',
-#             'filename': 'server.log'
-#         }
-#     },
-#     'loggers': {
-#         'testlogger': {
-#             'handlers': ['logfile'],
-#             'level': 'INFO',
-#         }
-#     }
-# }
-# DEBUG_PROPAGATE_EXCEPTIONS = True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'logfile': {
+            'class': 'logging.FileHandler',
+            'filename': 'server.log'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['logfile'],
+            'level': 'INFO',
+        }
+    }
+}
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 TINYMCE_JS_URL =  'https://onmodestaticfiles.s3.ir-thr-at1.arvanstorage.com/static/tinymce/tinymce.min.js'
