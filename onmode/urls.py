b9a91f61ec02aa29e017aca6385073143ff357d3
0x-m <m.ghaedi.74@gmail.com>
2022-05-28 21:05:46 +0430

    adding robots.txt

--- onmode/urls.py
+++ onmode/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import TemplateView
     
 from catalogue.views import shop, search, category
 
@@ -11,6 +11,7 @@ from catalogue.views import shop, search, category
 urlpatterns = [
     path('', include('index.urls', namespace='index')),
     path('', include('orders.urls', namespace='orders')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('tinymce/', include('tinymce.urls')),
     path('admin/', admin.site.urls),
     path('catalogue/', include('catalogue.urls', namespace='catalogue')),
