9901b22276e3116836abebca2f0d088c98552b64
0x-m <m.ghaedi.74@gmail.com>
2022-05-28 20:46:10 +0430

    sitemap disabled

--- onmode/settings_production.py
+++ onmode/settings_production.py
@@ -22,8 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
     'storages',
     'tinymce',
     'gtm',
